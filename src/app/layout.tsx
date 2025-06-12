01b51aec0813d42612b4214b6218e66b470ad6db
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2025-06-12 10:22:35 +0700

    feat: better toast positioning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,7 +44,7 @@ export default async function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-            <Toaster richColors />
+            <Toaster position="top-right" richColors />
 
             <AutoSigninAnon user={session?.user}>{children}</AutoSigninAnon>
           </ThemeProvider>
