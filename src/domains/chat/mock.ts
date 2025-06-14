705b9126e4307025690e65bf19bf53a38022b03d
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2025-06-14 10:47:12 +0700

    feat: better test data output

--- src/domains/chat/mock.ts
+++ src/domains/chat/mock.ts
@@ -1,34 +1,46 @@
+import { faker } from "@faker-js/faker";
 import { simulateReadableStream } from "ai";
 import { MockLanguageModelV1 } from "ai/test";
 
 export const getMockModelForGenerateText = () => {
   return new MockLanguageModelV1({
-    doGenerate: async () => ({
-      rawCall: { rawPrompt: null, rawSettings: {} },
-      finishReason: "stop",
-      usage: { promptTokens: 10, completionTokens: 20 },
-      text: `Hello, world!`,
-    }),
+    doGenerate: async () => {
+      const n = faker.number.int({ min: 1, max: 3 });
+      const loremIpsum = faker.lorem.words(n);
+
+      return {
+        rawCall: { rawPrompt: null, rawSettings: {} },
+        finishReason: "stop",
+        usage: { promptTokens: 10, completionTokens: 20 },
+        text: loremIpsum,
+      };
+    },
   });
 };
 
 export const getMockModelForStreamText = () => {
   return new MockLanguageModelV1({
-    doStream: async () => ({
-      stream: simulateReadableStream({
-        chunks: [
-          { type: "text-delta", textDelta: "Hello" },
-          { type: "text-delta", textDelta: ", " },
-          { type: "text-delta", textDelta: `world!` },
-          {
-            type: "finish",
-            finishReason: "stop",
-            logprobs: undefined,
-            usage: { completionTokens: 10, promptTokens: 3 },
-          },
-        ],
-      }),
-      rawCall: { rawPrompt: null, rawSettings: {} },
-    }),
+    doStream: async () => {
+      const n = faker.number.int({ min: 1, max: 10 });
+      const loremIpsum = faker.lorem.paragraphs(n);
+
+      return {
+        stream: simulateReadableStream({
+          chunks: [
+            ...loremIpsum.split(" ").map((word) => ({
+              type: "text-delta" as const,
+              textDelta: `${word} `,
+            })),
+            {
+              type: "finish",
+              finishReason: "stop",
+              logprobs: undefined,
+              usage: { completionTokens: 10, promptTokens: 3 },
+            },
+          ],
+        }),
+        rawCall: { rawPrompt: null, rawSettings: {} },
+      };
+    },
   });
 };
